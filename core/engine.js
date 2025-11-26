b02a70161a28dc2b003f5f625825cd603c5d9c03
Ashya <128183143+ashyapeachie@users.noreply.github.com>
2025-11-25 20:15:08 -0500

    writing constructor with an if statement

--- core/engine.js
+++ core/engine.js
@@ -1,13 +1,24 @@
 /* 
 author: Aham
 filename: engine.js
-description: 
+description: program
 */
 
 export class Engine {
-    // constructor
+    constructor(gl, vertexShader, fragmentShader) {
+        this.gl = gl;
+        this.program = gl.createProgram();
+
+        gl.attachShader(this.program, vertexShader.shader);
+        gl.attachShader(this.program, fragmentShader.shader);
+        gl.linkProgram(this.program);
+
+        if(!gl.getProgramParameter(this.program, gl.LINK_STATUS)) {
+            throw new Error("Program link error: " + gl.getProgramInfoLog(this.program));
+        }
+    }
 
     // use
-    
+
     // uniform 
 }
