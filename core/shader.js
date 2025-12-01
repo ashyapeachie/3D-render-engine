a3264484986f0e65c195cd5d3ff5049a4a281f0f
Ashya <128183143+ashyapeachie@users.noreply.github.com>
2025-11-30 21:24:55 -0500

    commenting possible code

--- core/shader.js
+++ core/shader.js
@@ -6,6 +6,14 @@ description: shader class
 
 export class Shader {
     constructor() {
-        
+        // this.gl = gl;
+        // this.shader = gl.createShader(type);
+        // gl.shaderSource();
+        // gl.compileShader();
+
+        /* if (!gl.getShaderParameter(this.shader, gl.COMPILE_STATUS)) {
+            throw new Error("Shader compile error: " + gl.getShaderInfoLog(this.shader));
+        } 
+        */
     }
 }
