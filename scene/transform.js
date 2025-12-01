a2a83c0c80747fbd84a15dca3ffa74f5b698376e
Ashya <128183143+ashyapeachie@users.noreply.github.com>
2025-11-30 21:48:06 -0500

    commenting code for constructor and updateMatrix

--- scene/transform.js
+++ scene/transform.js
@@ -7,7 +7,20 @@ description: transform component
 // import
 
 export class Transform {
-    // constructor() {}
+    constructor() {
+        // this.position = [0, 0, 0];
+        // this.rotation = [0, 0, 0];
+        // this.scale = [1, 1, 1];
+        // this.modelMatrix = mat4.create();
+    }
 
-    // updateMatrix() {}
+    updateMatrix() {
+        const m = this.modelMatrix;
+        // mat4.identity(m);
+        // mat4.translate(m, m, this.position);
+        // mat4.rotateX(m, m, this.rotation[0]);
+        // mat4.rotateY(m, m, this.rotation[1]);
+        // mat4.rotateZ(m, m, this.rotation[2]);
+        // mat4.scale(m, m, this.scale);
+    }
 }
