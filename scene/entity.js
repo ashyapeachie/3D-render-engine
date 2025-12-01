fc92e50d55589e5c150e205658ed2bc4f852143a
Ashya <128183143+ashyapeachie@users.noreply.github.com>
2025-11-30 21:49:45 -0500

    adding to constructor, update, and draw

--- scene/entity.js
+++ scene/entity.js
@@ -5,15 +5,18 @@ description: entity class
 */
 
 export class Entity {
-    constructor() {
-
+    constructor(mesh, material) {
+        this.mesh = mesh;
+        this.material = material;
+        this.transform = new Transform();
     }
 
     update() {
-
+         this.transform.updateMatrix();
     }
 
-    draw() {
-        
+    draw(renderer, camera) {
+        this.material.bind(this.transform.modelMatrix, camera);
+        this.mesh.draw();
     }
 }
